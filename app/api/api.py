7480c2e5abc0f49cd501c7e2d9275fb380f3c880
Felipe Allegretti <felipeallh@gmail.com>
2023-03-02 17:35:15 -0300

    Implement web application (#1)
    * Setup web API
    
    * Change to jpg format
    
    * Return PDF as buffer
    
    * Add Bad Request handler
    
    * Update app structure
    
    * Format flake8
    
    * Implement GitHub Actions workflow

---
+++ app/api/api.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from app.services.resumeio import ResumeioDownloader
+
+api_router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@api_router.get("/download", response_class=HTMLResponse)
+def download_resume(resume_id: str):
+    resumeio = ResumeioDownloader(resume_id=resume_id, image_size=3000, extension="jpg")
+    buffer = resumeio.run()
+    return Response(buffer, headers={"Content-Disposition": 'inline; filename="out.pdf"'}, media_type="application/pdf")
+
+
+@api_router.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
---
+++ app/services/resumeio.py
@@ -0,0 +1,77 @@
+import json
+import re
+from dataclasses import dataclass, field
+from datetime import datetime
+
+import requests
+from fastapi import HTTPException
+from fpdf import FPDF
+
+
+@dataclass
+class ResumeioDownloader:
+    resume_id: str
+
+    extension: str = "png"
+    image_size: int = 1800
+    cache_date: str = datetime.utcnow().isoformat()[:-4] + "Z"
+
+    images_urls: list = field(default_factory=lambda: [])
+
+    IMAGE_URL: str = "https://ssr.resume.tools/to-image/ssid-{0}-{1}.{2}?cache={3}&size={4}"
+    METADATA_URL: str = "https://ssr.resume.tools/meta/ssid-{0}?cache={1}"
+
+    def __post_init__(self):
+        pattern_id = re.compile(r"^[a-zA-Z0-9]{9}$")
+        pattern_url = re.compile(r"(?<=resume.io/r/)([a-zA-Z0-9]){9}")
+
+        if pattern_id.search(self.resume_id):
+            pass
+
+        elif pattern_url.search(self.resume_id):
+            self.resume_id = pattern_url.search(self.resume_id).group(0)
+
+        else:
+            raise HTTPException(status_code=400, detail=f"Invalid resume id: {self.resume_id}")
+
+    def run(self) -> bytearray:
+        self._get_resume_metadata()
+        self._format_images_urls()
+        self._generate_pdf()
+
+        return self.buffer
+
+    def _get_resume_metadata(self):
+        request = requests.get(self.METADATA_URL.format(self.resume_id, self.cache_date))
+        metadata = json.loads(request.text)
+        metadata = metadata.get("pages")
+        self.metadata = metadata
+
+    def _format_images_urls(self):
+        for page_id in range(1, 1 + len(self.metadata)):
+            download_url = self.IMAGE_URL.format(
+                self.resume_id,
+                page_id,
+                self.extension,
+                self.cache_date,
+                self.image_size,
+            )
+            self.images_urls.append(download_url)
+
+    def _generate_pdf(self):
+        w, h = self.metadata[0].get("viewport").values()
+
+        pdf = FPDF(format=(w, h))
+        pdf.set_auto_page_break(0)
+
+        for i, image_url in enumerate(self.images_urls):
+            pdf.add_page()
+            pdf.image(image_url, w=w, h=h, type=self.extension)
+
+            for link in self.metadata[i].get("links"):
+                x = link["left"]
+                y = h - link["top"]
+
+                pdf.link(x=x, y=y, w=link["width"], h=link["height"], link=link["url"])
+
+        self.buffer = pdf.output(dest="S")
