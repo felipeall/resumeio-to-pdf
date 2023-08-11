3225763f1867d29564c57b9e0c89fcf8ce58d677
Felipe Allegretti <felipeallh@gmail.com>
2023-08-11 10:38:20 -0300

    General formatting and docstrings (#3)

--- app/api/api.py
+++ app/api/api.py
@@ -10,11 +10,39 @@ templates = Jinja2Templates(directory="templates")
 
 @api_router.get("/download", response_class=HTMLResponse)
 def download_resume(resume_id: str):
+    """
+    Download a resume from resume.io and return it as a PDF.
+
+    Parameters
+    ----------
+    resume_id : str
+        ID or URL of the resume to download.
+
+    Returns
+    -------
+    fastapi.responses.Response
+        A PDF representation of the resume with appropriate headers for inline display.
+    """
     resumeio = ResumeioDownloader(resume_id=resume_id, image_size=3000, extension="jpg")
     buffer = resumeio.run()
-    return Response(buffer, headers={"Content-Disposition": 'inline; filename="out.pdf"'}, media_type="application/pdf")
+    return Response(
+        buffer, headers={"Content-Disposition": 'inline; filename="resume.pdf"'}, media_type="application/pdf"
+    )
 
 
 @api_router.get("/", response_class=HTMLResponse)
 def index(request: Request):
+    """
+    Render the main index page.
+
+    Parameters
+    ----------
+    request : fastapi.Request
+        The request instance.
+
+    Returns
+    -------
+    fastapi.templating.Jinja2Templates.TemplateResponse
+        Rendered template of the main index page.
+    """
     return templates.TemplateResponse("index.html", {"request": request})
