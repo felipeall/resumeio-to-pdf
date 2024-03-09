6b06e6e523fc8ea7318e1682b8e1720dd64e582e
Felipe Allegretti <felipeallh@gmail.com>
2024-03-09 16:23:23 -0300

    refactor: Implement new API router

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.api.api import api_router
+from app.api.api import router
 
 app = FastAPI(title="Resume.io to PDF")
-app.include_router(api_router)
+app.include_router(router)
 
 
 if __name__ == "__main__":
     """Instantiate the application webserver"""
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["app", "templates"])
