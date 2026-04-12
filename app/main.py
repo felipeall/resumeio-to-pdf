302d0fb45cbcc86f0d9d95e59820cfb3d4093c43
Felipe Allegretti <felipeallh@gmail.com>
2026-04-12 12:15:56 +0200

    disable hot reload

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(router)
 
 if __name__ == "__main__":
     """Instantiate the application webserver"""
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["app", "templates"])
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
